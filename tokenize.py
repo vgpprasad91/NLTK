99e42d09b0b8dd144a49149c636a3500ca51c416
V.GURU PRASAD <vguruprasad91@gmail.com>
2016-04-11 11:59:23 -0400

    Create tokenize.py

---
+++ tokenize.py
@@ -0,0 +1,18 @@
+#imort nltk 
+
+import nltk
+nltk.download()
+
+# Tokenize sentences
+from nltk.tokenize import sent_tokenize, word_tokenize
+Sample_text = " Hello I am Mr. Guruprasad. I am looking for a NLP job."
+print (sent_tokenize(Sample_text))
+
+# Output : [' Hello I am Mr. Guruprasad.', 'I am looking for a NLP job.']
+
+# Tokenize words 
+print (word_tokenize(Sample_text))
+
+# Output : ['Hello', 'I', 'am', 'Mr.', 'Guruprasad', '.', 'I', 'am', 'looking', 'for', 'a', 'NLP', 'job', '.']
+
+
