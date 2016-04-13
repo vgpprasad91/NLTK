7b37bb2d79b36a2e61421128631e6e20df98dc88
V.GURU PRASAD <vguruprasad91@gmail.com>
2016-04-13 18:40:46 -0400

    Create stopwords2.py

---
+++ stopwords2.py
@@ -0,0 +1,10 @@
+#stopwords filtering
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+stopwordlist = set(stopwords.words('english'))
+Sample_text = "So, I thought I can prove my skills in NLP by writing programs using NLTK toolkit."
+splitwordlist = word_tokenize(Sample_text)
+print([w for w in splitwordlist if w not in stopwordlist])
+
+#output : ['So', ',', 'I', 'thought', 'I', 'prove', 'skills', 'NLP', 'writing', 'programs', 'using', 'NLTK', 'toolkit', '.']
