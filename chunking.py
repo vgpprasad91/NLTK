a8b6912ab2d5a594787f2170fca0c96eb8c2d715
V.GURU PRASAD <vguruprasad91@gmail.com>
2016-04-13 18:35:02 -0400

    Update chunking.py

--- chunking.py
+++ chunking.py
@@ -18,11 +18,11 @@ train_tokenizer = PunktSentenceTokenizer(train_data)
 regexpar = nltk.RegexpParser(r"""Chunk: {<.*>+}
                                          }<NNP.?|IN|NNS|TO>+{""")
 
-def chunking():
+def chinking():
     try:
         print([regexpar.parse(nltk.pos_tag(nltk.word_tokenize(w))).draw() for w in train_tokenizer.tokenize(sample_data)])
 
     except Exception as e:
         print(str(e))
 
-chunking()
+chinking()
